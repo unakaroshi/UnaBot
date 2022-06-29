14ac624d2de82728f6ba4c0ee47c4231da371973
Michael Roehrl <michael.roehrl@gmx.de>
2022-06-29 20:14:30 +0200

    Help command now is up to date

--- UnaBot.py
+++ UnaBot.py
@@ -2,8 +2,6 @@
 import os
 import discord
 from discord.ext import commands
-#from discord.ext import discord
-
 import DiceHandler
 from dotenv import load_dotenv
 
@@ -11,6 +9,8 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+
+
 
 @bot.event
 async def on_ready():
@@ -25,8 +25,32 @@ async def on_member_join(member):
 
 @bot.command(name='unahelp')
 async def unahelp(ctx):
-    response = "Try !rd 5w6"
-    await ctx.send(response) 
+    embed=discord.Embed(title="Help for the fantastic UnaBot", 
+                        description=
+                        """
+                        **__!rd-Command (roll dice)__**
+                          Roll a various number (1-100) of n-sided dices.
+                          Only DND style dices are allowed (D4, D6, D8, D10, D12, D20 and D100)
+                          
+                          The parameter consists of the number of dices the letter "d" (for 
+                          dice) or "w" (for Würfel) and the number of sides the dices should 
+                          have. Here are some examples:
+                           !rd 5w6
+                           !rd 5d6
+                           !rd 8d10
+                           !rd 1w100
+                           
+                        **__!ase-Commond (AttribSchwerExtrem)__**
+                        For Cthulu RPG you sometimes need a half or a fifth of the
+                        value of you attribute. This function helps you calculate 
+                        this. For example:
+                           !ase 99
+                           !ase 50
+                           !ase 66
+                        """, 
+                        color=0x22A7F0)
+    #response = "Try !rd 5w6"
+    await ctx.send(embed=embed) 
 
 
     
@@ -50,19 +74,23 @@ async def rollDice(ctx,arg):
     
     if dice_sides not in available_dice_sides:
         await ctx.send("Only D4, D6, D8, D10, D12, D20 and D100 are allowed")
-        return;
+        return
     
-    response = DiceHandler.handleDiceRequest(dice_number, dice_sides)
+    dicerolls = DiceHandler.rollDices(dice_number, dice_sides)
     dice_name = "D{}".format(dice_sides)
-    embed=discord.Embed(title="{} rolls {} {}".format(ctx.author.display_name, dice_number, dice_name), description=response, color=0x22A7F0)
+    embed=discord.Embed(title="{} rolls {} {}".format(ctx.author.display_name, dice_number, dice_name), 
+                        description=", ".join(str(elem) for elem in dicerolls), 
+                        color=0x22A7F0)
+    
     await ctx.send(embed=embed)
-    #await ctx.send(response) 
+    
 
 @bot.command(name="ase")
 async def ASE(ctx,arg):
     difficult = int(int(arg)//2)
     extrem = int(int(arg)//5)
     await ctx.send("Schwer: {}   Extrem: {}".format(difficult, extrem))
+        
         
 @bot.listen()
 async def on_member_update(before, after):
